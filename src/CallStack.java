01331af286b4728a737116d3c2d7a8c3d3cbb69f
Sukhvindra9718 <sukhvindra.singh@resilientitservices.com>
2022-07-14 20:02:56 +0530

    Sukhvindra | Changes in Stack and Call Stack

--- src/CallStack.java
+++ src/CallStack.java
@@ -1,16 +1,21 @@
 public class CallStack {
     public static void main(String[] args) {
         Stack stack = new Stack();
-
+        // LIFO
+        
         stack.push(10);
         stack.push(20);
         stack.push(30);
-        stack.push(40);
-
-        stack.pop();
-        stack.pop();
-
+        
         
         stack.top();
+        stack.pop();
+
+        stack.top();
+        stack.pop();
+        
+        stack.pop();
+
+
     }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,20 +1,32 @@
 public class Stack {
-    private int top = 0;
-    private int[] arr = new int[20];
+    private int top = -1;
+    private int[] arr = new int[2];
 
     public void push(int data)
     {
-        arr[top++] = data;
-        System.out.println("Element pushed successfully");
+        if(top == arr.length-1){
+            System.out.println("Stack is full");
+        }else{
+            arr[++top] = data;
+            System.out.println("Element pushed successfully");
+        }
     }
     public void pop()
     {
-        int data = arr[--top];
-        System.out.println("Your popped Element is : " + data);
+        if(top >= 0){
+            int data = arr[top--];
+            System.out.println("Your popped Element is : " + data);
+        }else{
+            System.out.println("Stack is empty");
+        }
     }
     public void top()
-    {
-        int data = arr[--top];
-        System.out.println("Your Top Element is : " + data);
+    {   
+        if(top >= 0){
+            int data = arr[top];
+            System.out.println("Your Top Element is : " + data);
+        }else{
+            System.out.println("Stack is empty");
+        }
     }
 }
